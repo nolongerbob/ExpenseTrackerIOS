bcb95a50f82cd4f046312d27fb37d5aafd78a173
Daniel Kozhemiakin <daniel.kojemyakin@icloud.com>
2025-12-12 15:15:51 +0300

    Initial Commit

---
+++ ExpenceTracker/ExpenceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenceTrackerApp.swift
+//  ExpenceTracker
+//
+//  Created by Daniel Kozhemiakin on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpenceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
